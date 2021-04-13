8fa85f18507ed08027bd38af823a9f96fedc65d3
Janik Schumann <janik-schumann@web.de>
2021-04-13 10:52:24 +0200

    Renamed files, added html, css and js file for search-results page.

---
+++ homepage.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function () {
+
+  var search_field_form = document.getElementById("search_field_form");
+  var search_input = document.getElementById("search_field_input");
+  var clear_icon = document.getElementById("clear_icon");
+  var button = document.getElementsByTagName("button")[1];
+  var header = document.getElementById("header");
+  var content = document.getElementById("content");
+  var footer = document.getElementById("footer");
+
+  search_field_form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  });
+
+  window.addEventListener("keyup", check_input);
+  window.addEventListener("keydown", check_input);
+  clear_icon.addEventListener("click", clear_input);
+  button.addEventListener("click", result_page);
+
+  function check_input() {
+    if (search_input.value == null || search_input.value == "") {
+      clear_icon.style.display = "none";
+    } else {
+      if (event.keyCode === 13) {
+        result_page();
+        return;
+      }
+      clear_icon.style.display = "block";
+    }
+  }
+
+  function clear_input() {
+    search_input.value = "";
+    clear_icon.style.display = "none";
+  }
+
+  function result_page() {
+    window.location.href = "http://127.0.0.1:3000/google-homepage/search-results.html";
+  }
+
+});
